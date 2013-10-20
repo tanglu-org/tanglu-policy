b1be62858f0e5dd71fa7fe247c48a0b6a9b39885
Matthias Klumpp <matthias@tenstral.net>
2013-10-20 15:46:43 +0200

    Make building policy very simple

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-cd ./publican-tanglu
-./build-brand.sh
-cd ..
+#!/bin/bash
+set -e
+
+mkdir -p build
+cd build
+rm -rf en
+cp -dpr ../source ./en
+cp ../publican.cfg .
 publican build --formats=html --langs=en
+rm -rf ../output
+mv tmp/en ../output
+cd ..
